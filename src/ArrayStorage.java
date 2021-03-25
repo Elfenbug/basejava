62ad80ce8115fe0cad8142df146f7022fe3e94fc
Elfenbug <elfenbug@gmail.com>
2021-03-25 15:39:11 +0500

    first_commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,28 +14,26 @@ public class ArrayStorage {
     void save(Resume r) {
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
-                // good result: found an empty slot
                 storage[i] = r;
                 positionInArray = i + 1;
                 return;
             }
             if (storage[i].uuid.equals(r.uuid)) {
-                // bad result: found a duplicate
-                System.out.println("ERROR: Resume with " + storage[i].uuid + " is already exist!");
+                System.out.println("Резюме с " + storage[i].uuid + " уже существует!");
                 return;
             }
         }
-        // bad result: storage full
-        System.out.println("ERROR: Storage full");
+        System.out.println("Массив заполнен");
     }
 
     Resume get(String uuid) {
-        Resume resume = new Resume();
         for (int i = 0; i < positionInArray; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                resume = storage[i];
+                return storage[i];
             }
         }
+        Resume resume = new Resume();
+        resume.uuid = "Резюме отсутствует";
         return resume;
     }
 
@@ -43,6 +41,7 @@ public class ArrayStorage {
         for (int i = 0; i < positionInArray; i++) {
             if(storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
+                positionInArray--;
             }
         }
         for (int i = 1; i < storage.length-1; i++) {
@@ -55,12 +54,25 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return storage;
+        int i = 0;
+        int j = storage.length - 1;
+        while (i <= j) {
+            if (storage[j] == null) {
+                j--;
+            } else if (storage[i] != null) {
+                i++;
+            } else {
+                storage[i] = storage[j];
+                storage[j] = null;
+                i++; j--;
+            }
+        }
+        return Arrays.copyOfRange(storage, 0, i);
     }
 
     int size() {
         int count = 0;
-        for (int i = 0; i < storage.length; i++) {
+        while (storage[count] != null) {
             count++;
         }
         return count;
