c43ac85b4747686779ab1c84d9a6173e2771ac11
Elfenbug <elfenbug@gmail.com>
2021-04-22 16:57:33 +0500

    HW5_first_commit

---
+++ test/com/basejava/webapp/storage/AbstractStorageTest.java
@@ -0,0 +1,37 @@
+package com.basejava.webapp.storage;
+
+import org.junit.Test;
+
+public class AbstractStorageTest {
+
+    public AbstractStorageTest(ListStorageTest listStorageTest) {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void size() {
+    }
+}
---
+++ test/com/basejava/webapp/storage/ListStorageTest.java
@@ -0,0 +1,8 @@
+package com.basejava.webapp.storage;
+
+public class ListStorageTest extends AbstractStorageTest {
+
+    protected ListStorageTest() {
+        super(new ListStorageTest());
+    }
+}
