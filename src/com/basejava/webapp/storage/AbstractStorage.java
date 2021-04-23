e0b9522792fbe4e989eb012471faec3f16a6d21d
Elfenbug <elfenbug@gmail.com>
2021-04-23 18:09:52 +0500

    HW5_first_commit

--- src/com/basejava/webapp/storage/AbstractStorage.java
+++ src/com/basejava/webapp/storage/AbstractStorage.java
@@ -1,40 +1,69 @@
 package com.basejava.webapp.storage;
 
+import com.basejava.webapp.exception.ExistStorageException;
+import com.basejava.webapp.exception.NotExistStorageException;
+import com.basejava.webapp.exception.StorageException;
 import com.basejava.webapp.model.Resume;
 
-public class AbstractStorage implements Storage {
-    @Override
-    public void clear() {
+import java.util.Arrays;
 
-    }
+public abstract class AbstractStorage implements Storage {
+    protected static final int STORAGE_LIMIT = 10_000;
+    protected Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected int count = 0;
 
-    @Override
-    public void update(Resume resume) {
+    protected abstract int getStorageIndex(String uuid);
 
-    }
+    protected abstract void insert(Resume resume, int index);
 
-    @Override
     public void save(Resume resume) {
-
+        int index = getStorageIndex(resume.getUuid());
+        if (index >= 0) {
+            throw new ExistStorageException(resume.getUuid());
+        } else if (count == STORAGE_LIMIT) {
+            throw new StorageException("Storage overflow", resume.getUuid());
+        } else {
+            insert(resume, index);
+            count++;
+        }
     }
 
-    @Override
-    public Resume get(String uuid) {
-        return null;
-    }
-
-    @Override
     public void delete(String uuid) {
-
+        int index = getStorageIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        if (count - index >= 0) {
+            System.arraycopy(storage, index + 1, storage, index, count - index);
+            count--;
+        }
     }
 
-    @Override
+    public void update(Resume resume) {
+        int index = getStorageIndex(resume.getUuid());
+        if (index >= 0) {
+            storage[index] = resume;
+        } else {
+            throw new NotExistStorageException(resume.getUuid());
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getStorageIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        return storage[index];
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, count);
     }
 
-    @Override
     public int size() {
-        return 0;
+        return count;
     }
 }
