6e409b9c875aeabd7b1f6a067d79dbb2a6f2a6b3
Elfenbug <elfenbug@gmail.com>
2021-06-07 17:34:20 +0500

    HW6_second_commit

--- src/com/basejava/webapp/ResumeTestData.java
+++ src/com/basejava/webapp/ResumeTestData.java
@@ -1,14 +1,50 @@
 package com.basejava.webapp;
 
-import com.basejava.webapp.model.ContactType;
-import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.model.*;
 import com.basejava.webapp.storage.ArrayStorage;
+
+import java.time.LocalDate;
+import java.util.EnumMap;
+import java.util.Map;
+
+import static java.time.LocalDate.now;
 
 public class ResumeTestData {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume("firstName1");
+        TextSection r1TextPosition = new TextSection("Ведущий стажировок и корпоративного обучения по Java Web и Enterprise технологиям");
+        TextSection r1TextPersonalQualities = new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры.");
+        TextSection r1Achievements = new TextSection("С 2013 года: разработка проектов \"Разработка Web приложения\",\"Java Enterprise\", \"Многомодульный maven. Многопоточность. XML (JAXB/StAX). Веб сервисы (JAX-RS/SOAP). Удаленное взаимодействие (JMS/AKKA)\". Организация онлайн стажировок и ведение проектов. Более 1000 выпускников.\n" +
+                "Реализация двухфакторной аутентификации для онлайн платформы управления проектами Wrike. Интеграция с Twilio, DuoSecurity, Google Authenticator, Jira, Zendesk.\n" +
+                "Налаживание процесса разработки и непрерывной интеграции ERP системы River BPM. Интеграция с 1С, Bonita BPM, CMIS, LDAP. Разработка приложения управления окружением на стеке: Scala/Play/Anorm/JQuery. Разработка SSO аутентификации и авторизации различных ERP модулей, интеграция CIFS/SMB java сервера.\n" +
+                "Реализация c нуля Rich Internet Application приложения на стеке технологий JPA, Spring, Spring-MVC, GWT, ExtGWT (GXT), Commet, HTML5, Highstock для алгоритмического трейдинга.\n" +
+                "Создание JavaEE фреймворка для отказоустойчивого взаимодействия слабо-связанных сервисов (SOA-base архитектура, JAX-WS, JMS, AS Glassfish). Сбор статистики сервисов и информации о состоянии через систему мониторинга Nagios. Реализация онлайн клиента для администрирования и мониторинга системы по JMX (Jython/ Django).\n" +
+                "Реализация протоколов по приему платежей всех основных платежных системы России (Cyberplat, Eport, Chronopay, Сбербанк), Белоруcсии(Erip, Osmp) и Никарагуа.");
+        TextSection r1Qualification = new TextSection("JEE AS: GlassFish (v2.1, v3), OC4J, JBoss, Tomcat, Jetty, WebLogic, WSO2\n" +
+                "Version control: Subversion, Git, Mercury, ClearCase, Perforce\n" +
+                "DB: PostgreSQL(наследование, pgplsql, PL/Python), Redis (Jedis), H2, Oracle,\n" +
+                "MySQL, SQLite, MS SQL, HSQLDB\n" +
+                "Languages: Java, Scala, Python/Jython/PL-Python, JavaScript, Groovy,\n" +
+                "XML/XSD/XSLT, SQL, C/C++, Unix shell scripts,\n" +
+                "Java Frameworks: Java 8 (Time API, Streams), Guava, Java Executor, MyBatis, Spring (MVC, Security, Data, Clouds, Boot), JPA (Hibernate, EclipseLink), Guice, GWT(SmartGWT, ExtGWT/GXT), Vaadin, Jasperreports, Apache Commons, Eclipse SWT, JUnit, Selenium (htmlelements).\n" +
+                "Python: Django.\n" +
+                "JavaScript: jQuery, ExtJS, Bootstrap.js, underscore.js\n" +
+                "Scala: SBT, Play2, Specs2, Anorm, Spray, Akka\n" +
+                "Технологии: Servlet, JSP/JSTL, JAX-WS, REST, EJB, RMI, JMS, JavaMail, JAXB, StAX, SAX, DOM, XSLT, MDB, JMX, JDBC, JPA, JNDI, JAAS, SOAP, AJAX, Commet, HTML5, ESB, CMIS, BPMN2, LDAP, OAuth1, OAuth2, JWT.\n" +
+                "Инструменты: Maven + plugin development, Gradle, настройка Ngnix,\n" +
+                "администрирование Hudson/Jenkins, Ant + custom task, SoapUI, JPublisher, Flyway, Nagios, iReport, OpenCmis, Bonita, pgBouncer.\n" +
+                "Отличное знание и опыт применения концепций ООП, SOA, шаблонов проектрирования, архитектурных шаблонов, UML, функционального программирования\n" +
+                "Родной русский, английский \"upper intermediate\"");
+        ExperienceWork r1ExperienceWork = new ExperienceWork("http://javaops.ru/", 10/2013, 06/2021, "Автор проекта.",
+                "Создание, организация и проведение Java онлайн проектов и стажировок.");
+        ContactType r1Contact = ContactType.;
+        SectionType r1Section = SectionType.
+
+        Map<ContactType, String> contacts = new EnumMap<>(r1Contact);
+        Map<SectionType, AbstractSection> sections = new EnumMap<>(SectionType.class);
+
         ARRAY_STORAGE.save(r1);
         r1.getContact(ContactType.PHONE);
         System.out.println();
